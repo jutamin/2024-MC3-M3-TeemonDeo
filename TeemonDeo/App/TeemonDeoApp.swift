1dcb433429b9d3cfeac6cc45502548ff136835cf
TEO <ktw1421@naver.com>
2024-07-23 15:10:43 +0900

    [START] First Commit

---
+++ TeemonDeo/App/TeemonDeoApp.swift
@@ -0,0 +1,32 @@
+//
+//  TeemonDeoApp.swift
+//  TeemonDeo
+//
+//  Created by TEO on 7/23/24.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct TeemonDeoApp: App {
+
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}

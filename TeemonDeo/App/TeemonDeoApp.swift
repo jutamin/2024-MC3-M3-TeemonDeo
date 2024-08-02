92dd6f73d37f1ee267201efd760f9f0141cfb8f0
KiTaeUk <ktw1421@naver.com>
2024-08-02 23:41:46 +0900

    [Fix] Lottie 파일2개 추가, challenge 파일 수정

--- TeemonDeo/App/TeemonDeoApp.swift
+++ TeemonDeo/App/TeemonDeoApp.swift
@@ -25,8 +25,8 @@ struct TeemonDeoApp: App {
     var body: some Scene {
         WindowGroup {
             NavigationView {
-                //RootView()
-                TimerView()
+                RootView()
+                //TimerView()
             }
         }
     }
